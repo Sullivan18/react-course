d73ec68e20d49cbcb852092e912b71876754c05a
Sullivan18 <andre.sullivan18@hotmail.com>
2024-07-27 22:45:58 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -3,23 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classname="App">
+      <h1>Olá React!</h1>
+      <p>Meu primeiro App</p>
     </div>
-  );
+    
+  )
 }
 
 export default App;
